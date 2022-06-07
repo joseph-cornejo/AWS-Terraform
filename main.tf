4fc83abe1e74838f3df05392452153ca90b699b4
Joseph Cornejo <joseph.cornejo@ymail.com>
2022-06-07 00:08:52 -0400

    Adding Networking Components for SWARM Testing

--- main.tf
+++ main.tf
@@ -44,6 +44,16 @@ resource "aws_internet_gateway" "dev-gw" {
   }
 }
 
+#define VPC for SWARM
+resource "aws_internet_gateway" "swarm-gw" {
+  vpc_id = aws_vpc.swarm-vpc.id
+
+  tags = {
+    Name = "Swarm"
+  }
+  
+}
+
 #Route Table for Gateway
 resource "aws_route_table" "dev_route_table" {
   vpc_id = aws_vpc.dev-vpc.id
@@ -58,6 +68,22 @@ resource "aws_route_table" "dev_route_table" {
   }
 }
 
+#Route Tables for Swarm Gateway
+resource "aws_route_table" "swarm_route_table" {
+  vpc_id = aws_vpc.swarm-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0./0"
+    gateway_id = aws_internet_gateway.swarm-gw.id
+  }
+
+  tags = {
+    Name = "Swarm"
+  }
+  
+}
+
+#subnet for dev VPC
 resource "aws_subnet" "dev_subnet" {
   vpc_id            = aws_vpc.dev-vpc.id
   cidr_block        = "10.0.1.0/24"
@@ -68,11 +94,29 @@ resource "aws_subnet" "dev_subnet" {
   }
 }
 
+#subnet for SWARM VPC
+resource "aws_subnet" "swarm_subnet" {
+  vpc_id = aws_vpc.swarm-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "name" = "Swarm"
+  }
+}
+
 #associate route table with subnet
 resource "aws_route_table_association" "dev_association" {
   subnet_id      = aws_subnet.dev_subnet.id
   route_table_id = aws_route_table.dev_route_table.id
 }
+
+#associate swarm route table with subnet
+resource "aws_route_table_association" "swarm_association" {
+  subnet_id = aws_subnet.swarm_subnet.id
+  route_table_id = aws_route_table.swarm_route_table.id
+}
+
 
 #create security group to restrict and allow only certain ports
 resource "aws_security_group" "allow_web" {
@@ -113,6 +157,48 @@ resource "aws_security_group" "allow_web" {
 
   tags = {
     Name = "allow_web"
+  }
+}
+
+#create security group to restrict and allow only certain ports to SWARM Gateway
+resource "aws_security_group" "swarm_allow_web" {
+  name        = "Swarm_allow_web_traffic"
+  description = "Swarm_allow web traffic"
+  vpc_id      = aws_vpc.swarm-vpc.id
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "swarm_allow_web"
   }
 }
 
