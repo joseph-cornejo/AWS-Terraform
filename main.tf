119a85d94c030b0be7104a4f361bc6b8cec38b94
Joseph Cornejo <joseph.cornejo@ymail.com>
2022-06-07 00:12:45 -0400

    Adding Networking Components for SWARM Testing

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_route_table" "swarm_route_table" {
   vpc_id = aws_vpc.swarm-vpc.id
 
   route {
-    cidr_block = "0.0.0.0./0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.swarm-gw.id
   }
 
