2c6d882143ca4fcf65c9e31fad9c2584df67ede1
Joseph Cornejo <joseph.cornejo@ymail.com>
2022-06-18 22:22:50 -0400

    main.tf

--- main.tf
+++ main.tf
@@ -24,17 +24,6 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
-#Define VPC for my Docker SWARM Test Environment
-resource "aws_vpc" "swarm-vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Environment = "swarm_environment"
-    Terraform   = "True"
-  }
-
-}
-
 #Gateway
 resource "aws_internet_gateway" "dev-gw" {
   vpc_id = aws_vpc.dev-vpc.id
@@ -42,17 +31,6 @@ resource "aws_internet_gateway" "dev-gw" {
   tags = {
     Name = "dev"
   }
-}
-
-
-#define GW for SWARM
-resource "aws_internet_gateway" "swarm-gw" {
-  vpc_id = aws_vpc.swarm-vpc.id
-
-  tags = {
-    Name = "Swarm"
-  }
-  
 }
 
 #Route Table for Gateway
@@ -69,21 +47,6 @@ resource "aws_route_table" "dev_route_table" {
   }
 }
 
-#Route Tables for Swarm Gateway
-resource "aws_route_table" "swarm_route_table" {
-  vpc_id = aws_vpc.swarm-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.swarm-gw.id
-  }
-
-  tags = {
-    Name = "Swarm"
-  }
-  
-}
-
 #subnet for dev VPC
 resource "aws_subnet" "dev_subnet" {
   vpc_id            = aws_vpc.dev-vpc.id
@@ -95,29 +58,12 @@ resource "aws_subnet" "dev_subnet" {
   }
 }
 
-#subnet for SWARM VPC
-resource "aws_subnet" "swarm_subnet" {
-  vpc_id = aws_vpc.swarm-vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    "name" = "Swarm"
-  }
-}
 
 #associate route table with subnet
 resource "aws_route_table_association" "dev_association" {
   subnet_id      = aws_subnet.dev_subnet.id
   route_table_id = aws_route_table.dev_route_table.id
 }
-
-#associate swarm route table with subnet
-resource "aws_route_table_association" "swarm_association" {
-  subnet_id = aws_subnet.swarm_subnet.id
-  route_table_id = aws_route_table.swarm_route_table.id
-}
-
 
 #create security group to restrict and allow only certain ports
 resource "aws_security_group" "allow_web" {
@@ -161,82 +107,12 @@ resource "aws_security_group" "allow_web" {
   }
 }
 
-#create security group to restrict and allow only certain ports to SWARM Gateway
-resource "aws_security_group" "swarm_allow_web" {
-  name        = "Swarm_allow_web_traffic"
-  description = "Swarm_allow web traffic"
-  vpc_id      = aws_vpc.swarm-vpc.id
-
-  ingress {
-    description = "HTTPS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    cidr_blocks = [ "10.0.1.0/24" ]
-    description = "Allow Communication Between Instances"
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "swarm_allow_web"
-  }
-}
-
 #network interface for web server
 resource "aws_network_interface" "web-server-nic" {
   subnet_id       = aws_subnet.dev_subnet.id
   private_ips     = ["10.0.1.50"]
   security_groups = [aws_security_group.allow_web.id]
 }
-
-#--------------------------------------network interfaces for SWARM Hosts------------------------------------------------
-resource "aws_network_interface" "swarm-nic-one" {
-  subnet_id       = aws_subnet.swarm_subnet.id
-  private_ips     = ["10.0.1.51"]
-  security_groups = [aws_security_group.swarm_allow_web.id]
-}
-
-resource "aws_network_interface" "swarm-nic-two" {
-  subnet_id       = aws_subnet.swarm_subnet.id
-  private_ips     = ["10.0.1.52"]
-  security_groups = [aws_security_group.swarm_allow_web.id]
-}
-
-resource "aws_network_interface" "swarm-nic-three" {
-  subnet_id       = aws_subnet.swarm_subnet.id
-  private_ips     = ["10.0.1.53"]
-  security_groups = [aws_security_group.swarm_allow_web.id]
-}
-#-------------------------------------- end of network interfaces for SWARM Hosts--------------------------------------------
 
 # Elastic IP for web server
 resource "aws_eip" "one" {
@@ -247,37 +123,6 @@ resource "aws_eip" "one" {
     aws_internet_gateway.dev-gw
   ]
 }
-
-#-------------------------------------- Elastic IPs for SWARM Hosts---------------------------------------------------
-
-resource "aws_eip" "swarm_one" {
-  vpc                       = true
-  network_interface         = aws_network_interface.swarm-nic-one.id
-  associate_with_private_ip = "10.0.1.51"
-  depends_on = [
-    aws_internet_gateway.swarm-gw
-  ]
-}
-
-resource "aws_eip" "swarm_two" {
-  vpc                       = true
-  network_interface         = aws_network_interface.swarm-nic-two.id
-  associate_with_private_ip = "10.0.1.52"
-  depends_on = [
-    aws_internet_gateway.swarm-gw
-  ]
-}
-
-resource "aws_eip" "swarm_three" {
-  vpc                       = true
-  network_interface         = aws_network_interface.swarm-nic-three.id
-  associate_with_private_ip = "10.0.1.53"
-  depends_on = [
-    aws_internet_gateway.swarm-gw
-  ]
-}
-#-------------------------------------- End of Elastic Ips for SWARM Hosts--------------------------------------------
-
 
 #--------------------------------------- Instances -------------------------------------------------------------------
 resource "aws_instance" "web-server-instance" {
@@ -303,81 +148,3 @@ resource "aws_instance" "web-server-instance" {
     Name = "web-server"
   }
 }
-
-
-resource "aws_instance" "Swarm-Host-One" {
-  ami               = "ami-09d56f8956ab235b3"
-  instance_type     = "t2.small"
-  availability_zone = "us-east-1a"
-  key_name          = "swarm-key"
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.swarm-nic-one.id
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update
-              sudo apt-get upgrade
-              sudo curl -sSL https://get.docker.com/ | sh              
-              EOF
-
-  tags = {
-    Name = "Swarm-Host-One"
-  }
-}
-
-resource "aws_instance" "Swarm-Host-Two" {
-  ami               = "ami-09d56f8956ab235b3"
-  instance_type     = "t2.small"
-  availability_zone = "us-east-1a"
-  key_name          = "swarm-key"
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.swarm-nic-two.id
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update
-              sudo apt-get upgrade
-              sudo curl -sSL https://get.docker.com/ | sh              
-              EOF
-
-  tags = {
-    Name = "Swarm-Host-Two"
-  }
-}
-
-resource "aws_instance" "Swarm-Host-Three" {
-  ami               = "ami-09d56f8956ab235b3"
-  instance_type     = "t2.small"
-  availability_zone = "us-east-1a"
-  key_name          = "swarm-key"
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.swarm-nic-three.id
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update
-              sudo apt-get upgrade
-              sudo curl -sSL https://get.docker.com/ | sh   
-              sudo groupadd docker
-              sudo usermod -aG docker $USER
-              newgrp docker           
-              EOF
-
-  tags = {
-    Name = "Swarm-Host-Three"
-  }
-}
-
-
-
-
-
