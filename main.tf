b678c8b75e7628bd03f2d4aefa5404d4e4cb0ff3
Joseph Cornejo <joseph.cornejo@ymail.com>
2022-06-27 20:43:09 -0400

    Adding linux environment, creating ec2 ubuntu server

--- main.tf
+++ main.tf
@@ -252,7 +252,7 @@ resource "aws_instance" "linux-server-instance" {
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
   key_name          = "Ubuntu"
-  root_device_name  = "/dev/xvda"
+
 
   network_interface {
     device_index         = 0
@@ -267,12 +267,5 @@ resource "aws_instance" "linux-server-instance" {
   tags = {
     Name = "linux-server"
   }
-
-  ebs_block_device {
-    device_name = "/dev/xvda"
-    snapshot_id = "snap-xxxxxxxx"
-    volume_size = 8
-    delete_on_termination = true
-    volume_type = "standard"
+  
   }
-}
