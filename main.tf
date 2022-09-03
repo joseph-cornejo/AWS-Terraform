fc712f987e7a7285265065dd76f58dc5cdba7369
Joseph Cornejo <joseph.cornejo@ymail.com>
2022-09-03 16:39:45 -0400

    adding db instance

--- main.tf
+++ main.tf
@@ -269,3 +269,32 @@ resource "aws_instance" "linux-server-instance" {
   }
 
   }
+
+
+#--------------------------------------- RDS -------------------------------------------------------------------
+  
+  resource "aws_db_parameter_group" "mybudget"{
+    name = "mybudget"
+    family = "mysql"
+
+    parameter {
+      name = "log_connections"
+      value = "1"
+    }
+  }
+  
+  resource "aws_db_instanace" "mybudget" {
+    identifier = "mybudget"
+    instance_class = "db.t3.micro"
+    allocated_storage = 15
+    engine = "mysql"
+    engine_version = "15.0"
+    username = "admin"
+    password = var.db_password
+    db_subnet_group_name = dev_subnet.name
+    vpc_security_group_ids = [aws_security_group.allow_linux_web.id]
+    parameter_group_name = aws_db_parameter_group.mybudget.name
+    publicly_accessible = false
+    skip_final_snapshot = true
+
+  }
