2df379cafaca264d736f4c45628ed425d12aeba9
Joseph Cornejo <joseph.cornejo@ymail.com>
2022-09-03 20:23:00 -0400

    adding rds subnets in multiple AZs

--- main.tf
+++ main.tf
@@ -274,14 +274,14 @@ resource "aws_instance" "linux-server-instance" {
 #--------------------------------------- RDS -------------------------------------------------------------------
 
 resource "aws_subnet" "rds_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.dev-vpc.id
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "rds_subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id     = aws_vpc.dev-vpc.id
+  cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1b"
 }
 
